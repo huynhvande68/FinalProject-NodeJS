a3dea669e5ccc76f5f5a1c124ba44d91bc08c2ca
BlackWolf2002 <86212381+hynvande@users.noreply.github.com>
2023-05-07 17:58:07 +0700

    connect db

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,9 +2,9 @@ const {HOST, USER, PASSWORD, DATABASE} = process.env;
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host : 'localhost',
-    user: 'root',
-    password: null,
-    database: 'nodejs-final',
+    host : HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
 });
 module.exports = connection;
